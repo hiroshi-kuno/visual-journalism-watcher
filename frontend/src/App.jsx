8591687a1d41bdc365657f095525147a4f8efb7a
KunouHiroshi <18419311+hiroshi-kuno@users.noreply.github.com>
2023-12-12 21:03:24 +0900

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,9 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import MainContainer from "./components/MainContainer";
 
 function App() {
-  const [list, setList] = useState([]);
-  useEffect(() => {
-    axios
-      .get(
-        "https://storage.googleapis.com/vrw-dataset/get_headers_raspi_update.json"
-        // "https://storage.googleapis.com/vrw-dataset/get_headline_update.json"
-      )
-      .then((response) => {
-        console.log(response.data);
-        setList(response.data);
-      })
-      .catch((error) => {
-        console.error("Error", error);
-      });
-  }, []);
-
   return (
-    <div className="container type-sans">
-      {list.map((block) => {
-        return (
-          <div className="container__block" key={block.date}>
-            <p className="container__block__date">{block.date}</p>
-            <ul className="container__block__list">
-              {block.updates.map((item) => {
-                return (
-                  <li className="list-item" key={item.head}>
-                    <div className="list-item__header">
-                      <p className="list-item__header__media">{item.media}</p>
-                      <p className="list-item__header__head">{item.head}</p>
-                    </div>
-                    <p className="list-item__title">{item.get_title}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        );
-      })}
+    <div className="container">
+      <MainContainer />
     </div>
   );
 }
---
+++ frontend/src/components/MainContainer.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function MainContainer() {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://storage.googleapis.com/vrw-dataset/get_headers_raspi_update.json"
+      )
+      .then((response) => {
+        // console.log(response.data);
+        setList(response.data);
+      })
+      .catch((error) => {
+        console.error("Error", error);
+      });
+  }, []);
+
+  return (
+    <div className="viewer type-sans">
+      {list.map((block) => {
+        return (
+          <div className="viewer__block" key={block.date}>
+            <p className="viewer__block__date">{block.date}</p>
+            <ul className="viewer__block__list">
+              {block.updates.map((item) => {
+                return (
+                  <li className="list-item" key={item.head}>
+                    <a
+                      href={item.url}
+                      className="list-item__link"
+                      target="_blank"
+                    >
+                      <div className="list-item__link__header">
+                        <p className="list-item__link__header__head">
+                          {item.head}
+                        </p>
+                        <p className="list-item__link__header__media">
+                          {item.media}
+                        </p>
+                      </div>
+                      <p className="list-item__link__title">{item.get_title}</p>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default MainContainer;
