6c5040fa828e98462faf9e4098e26aabf2ae867e
KunouHiroshi <18419311+hiroshi-kuno@users.noreply.github.com>
2024-03-10 22:25:11 +0900

    update

--- frontend/src/components/MainContainerWatchList.jsx
+++ frontend/src/components/MainContainerWatchList.jsx
@@ -1,32 +1,61 @@
 import { useContext, useEffect, useState } from "react";
 import { ContextData } from "../context/AppContext";
 
+const returnStatusClass = (item) => {
+  if (item.get_type === "disable") {
+    return "watch-list__status";
+  } else {
+    return item.get_status
+      ? "watch-list__status true"
+      : "watch-list__status false";
+  }
+};
+
 const MainContainerWatchList = () => {
   const [state, useState] = useContext(ContextData);
-  // console.log(state.dataReporters[0], state.dataSections[0]);
   return (
-    <div>
-      <h2>Reporters</h2>
+    <div className="watch-list">
+      <h2 className="watch-list__headline">Reporters</h2>
       {state.dataReporters.map((item) => {
+        console.log(item);
         return (
-          <div key={item.head}>
-            <a href={item.url} target="_blank">
+          <div className="watch-list__item" key={item.head}>
+            <div className={returnStatusClass(item)}></div>
+            <a
+              className="watch-list__item__link"
+              href={item.url}
+              target="_blank"
+            >
               {item.head}
             </a>
-            <p>{item.media}</p>
+            <p className="watch-list__item__media">{item.media}</p>
+            <a
+              className={
+                item.social
+                  ? "watch-list__item__social"
+                  : "watch-list__item__social disable"
+              }
+              href={item.social}
+            >
+              Social
+            </a>
           </div>
         );
       })}
 
-      <h2>Visual Sections</h2>
+      <h2 className="watch-list__headline">Visual Sections</h2>
       {state.dataSections.map((item) => {
         return (
-          <div key={item.url}>
-            <p>{item.media}</p>
-
-            <a href={item.url} target="_blank">
+          <div className="watch-list__item" key={item.url}>
+            <div className={returnStatusClass(item)}></div>
+            <a
+              className="watch-list__item__link"
+              href={item.url}
+              target="_blank"
+            >
               {item.head}
             </a>
+            <p className="watch-list__item__media">{item.media}</p>
           </div>
         );
       })}
